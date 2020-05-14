fe392157c11811bca4a2bb2eb2e73493d378b514
jiang5059 <3254845059@qq.com>
2020-05-14 20:37:06 +0800

    买家秀分页

--- src/main/java/com/zt/controller/BuyershowController.java
+++ src/main/java/com/zt/controller/BuyershowController.java
@@ -1,6 +1,7 @@
 package com.zt.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zt.entity.Buyershow;
@@ -72,7 +73,11 @@ public class BuyershowController {
     }
 
     @RequestMapping("/jlpageindex")
-    public String Jlpageindex(int first1,Model medel,HttpSession session1){
+    public String Jlpageindex(String first3,Model medel,HttpSession session1){
+        if(first3==null){
+            first3="0";
+        }
+        int first1 = Integer.parseInt(first3);
         //分页
         int pageSize=6;
         //查询页码
@@ -107,7 +112,7 @@ public class BuyershowController {
         accountlist.forEach(System.out::println);
         //存值
         medel.addAttribute("accountlist",accountlist);  //保存数组
-        medel.addAttribute("count",count);              //总页数
+        medel.addAttribute("carsunconnt",count);              //总页数
         medel.addAttribute("firstxs",first2);              //页码
 
         session1.setAttribute("first", first);  //实际页码
@@ -117,6 +122,51 @@ public class BuyershowController {
         return "carsun";
     }
 
+    @RequestMapping("/jlpageindex2")
+    @ResponseBody
+    public String Jlpageindex2(String first3,Model medel,HttpSession session1){
+        if(first3==null){
+            first3="0";
+        }
+        int first1 = Integer.parseInt(first3);
+        //分页
+        int pageSize=6;
+        //查询页码
+        System.out.println(first1);
+        int first=0;
+        if(first1!=0){
+            first=(first1-1)*pageSize;
+        }
+        //总页数
+        int count=buyershowService.getPageCount();
+        if(count<=0){
+            System.out.println("获取总页数失败！！！");
+            // return "redirect:index.jsp";
+        }
+        if(count%pageSize==0){
+            count=count/pageSize;
+        }else{
+            count=count/pageSize+1;
+        }
+        System.out.println("总页数"+count);
+
+        //显示页码
+        int first2=0;
+        if(first!=0){
+            first2=(first/pageSize+1);
+        }else{
+            first2=1;
+        }
+        System.out.println("显示页数"+first2);
+
+        List<Buyershow> accountlist= buyershowService.getPageIndex(first, pageSize);
+        accountlist.forEach(System.out::println);
+
+        //存值
+        String str=first2+"~"+count+"~"+ JSONArray.toJSONString(accountlist);
+        System.out.println(str);
+        return str;
+    }
 
 
 
